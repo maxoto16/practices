e23544793014d2b24d7acf2861f3da87dea5678c
maxoto16 <91856580+maxoto16@users.noreply.github.com>
2023-10-06 15:01:37 +0000

    Commind

--- Practica6.java
+++ Practica6.java
@@ -1,17 +1,42 @@
 public class Practica6 {
     public static void main(String[] args) {
         
-        // siclo for 
+        //CALCULAR EL FACTORIAL DE UN NUMERO
+        //MOSTRAR LA SERIE DE FIBONACCI
+        //DIBUJAR EN CONSOLA UN TRIANGULO DEPENDIENDO DE UN NUMERO N
 
-        for (int i = 0; i < 100; i++) {
 
-            if (i%2 == 0) {
-                System.out.println("i es par" + i); );
-                    
-                } else {
-                    System.out.println("i es impar" + i);
-                }
-            }
+
+        //FACTORIAL
+        int n = 5;
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+            System.out.println(factorial);
         }
-    }
 
+
+        //FIBONACCI
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        int nn = 10;
+        for (int i = 0; i < nn; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+
+            System.out.println(c);
+        }
+
+        //TRIANGULO
+        int nnn = 5;
+        for (int i = 0; i < nnn; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
---
+++ Practica9.java
@@ -0,0 +1,17 @@
+public class Practica9 {
+    public static void main(String[] args) {
+        //sucecion de fibonacci
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        int n = 10;
+        System.out.println("0");
+        System.out.println("1");
+        for (int i = 0; i < n; i++) {
+            c = a + b;
+            System.out.println(c);
+            a = b;
+            b = c;
+        }
+    }
+}
