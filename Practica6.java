69928654fa27a8f4a4f02a3480ae0bff3c908c5c
maxoto16 <91856580+maxoto16@users.noreply.github.com>
2023-10-06 14:33:07 +0000

    commind

---
+++ Practica6.java
@@ -0,0 +1,17 @@
+public class Practica6 {
+    public static void main(String[] args) {
+        
+        // siclo for 
+
+        for (int i = 0; i < 100; i++) {
+
+            if (i%2 == 0) {
+                System.out.println("i es par" + i); );
+                    
+                } else {
+                    System.out.println("i es impar" + i);
+                }
+            }
+        }
+    }
+
