585989a2e6e284cf1943217403d83bcd3e6b1f15
maxoto16 <91856580+maxoto16@users.noreply.github.com>
2023-10-09 19:53:11 +0000

    Commind

--- Practica7.java
+++ Practica7.java
@@ -1,12 +1,50 @@
+import java.util.Scanner;
+
 public class Practica7 {
     public static void main(String[] args) {
         //estrucutra while Calcular factorial
 
-        System.out.println("Factorial");
-        System.out.println("Ingregese un numero");
 
-        while 
 
-        
-    }
+
+
+
+
+
+
+
+
+
+
+//#############################################################################################
+
+Scanner sc = new Scanner(System.in);
+String continuar = "si";
+while (continuar.toLowerCase().equals("si"))
+{
+        System.out.println("Factorial  ");
+        System.out.println("Ingrese un numero...........  ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int factorial = 1;
+if (num < 0){}  
+else
+{
+        while (num >= 1) {
+            factorial *= num;
+            num--;
+        }
+        System.out.println("FACTORIAL"+ factorial);
+        System.out.println("El factorial del numero ingresado es: " + factorial);
+        if (!continuar.equals("si")){
+System.out.println("BYE ");
+        }
+        else {
+                   while (num >= 1) {
+            factorial *= num;
+            num--; 
+        }
 }
+    }
+    }}}
+
